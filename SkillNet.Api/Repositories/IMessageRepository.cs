dc12d6615d484b5b430e53c4b43f5d8ac8181df5
rafiabdulnadir <b22in068l@kitsw.ac.in>
2025-10-01 19:25:19 +0530

    Delete SkillNet.Api directory

--- SkillNet.Api/Repositories/IMessageRepository.cs
+++
@@ -1,9 +0,0 @@
-using SkillNet.Api.Models;
-
-namespace SkillNet.Api.Repositories;
-
-public interface IMessageRepository : IGenericRepository<Message>
-{
-    Task<IEnumerable<Message>> GetConversationMessagesAsync(int conversationId);
-    Task<IEnumerable<Conversation>> GetUserConversationsAsync(string userId);
-}
